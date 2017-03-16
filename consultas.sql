0ab81cc673e87d21e44d9a2130b3031a050bccfb
oswaldoadidas <oswaldoadidas@hotmail.com>
2017-03-16 12:19:21 -0600

    right join, full join and to know columns's name

--- consultas.sql
+++ consultas.sql
@@ -32,3 +32,20 @@ FROM cert_graduado
 LEFT JOIN cert_grado
 ON cert_graduado.num_cuenta = cert_grado.num_cuenta
 ORDER BY cert_graduado.a_paterno;
+
+
+SELECT cert_actividad_graduado.universidad,cert_graduado.email
+FROM cert_actividad_graduado
+RIGHT JOIN cert_graduado
+ON cert_actividad_graduado.num_cuenta = cert_graduado.num_cuenta
+ORDER BY cert_actividad_graduado.universidad
+
+
+select * from INFORMATION_SCHEMA.columns where column_name = 'espid' /*Buscar nombre de una columna en tablas de un BD*/
+
+
+SELECT cert_optitulacion.nombre, cert_grado.entidad
+FROM cert_optitulacion
+FULL OUTER JOIN cert_grado
+ON cert_optitulacion.opttid = cert_grado.opttid
+ORDER BY cert_optitulacion.opttid
